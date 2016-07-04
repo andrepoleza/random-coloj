89f77144a9b550f06a3f65a86ba4ea1f00f3c0f8
André Poleza <andrepoleza@nepente.io>
2016-07-04 19:32:56 -0300

    Adds random-coloj.core/define-color function

--- src/random_coloj/core.clj
+++ src/random_coloj/core.clj
@@ -1,6 +1,15 @@
 (ns random-coloj.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn define-color
+  ([name hue-range lower-bounds]
+   (define-color name hue-range lower-bounds '()))
+  ([name hue-range lower-bounds color-bounds]
+   (let [s-min (get-in lower-bounds [0 0])
+         s-max (get-in lower-bounds [(dec (count lower-bounds)) 0])
+         b-min (get-in lower-bounds [(dec (count lower-bounds)) 1])
+         b-max (get-in lower-bounds [0 1])]
+    (conj color-bounds {:name name
+                        :hue-range hue-range
+                        :lower-bounds lower-bounds
+                        :saturation-range [s-min s-max]
+                        :brightness-range [b-min b-max]}))))
--- test/random_coloj/core_test.clj
+++ test/random_coloj/core_test.clj
@@ -2,6 +2,99 @@
   (:require [clojure.test :refer :all]
             [random-coloj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest define-color-test
+  (testing "define-color: monochrome"
+    (let [color-bounds '()
+          monochrome {:name "monochrome"
+                      :hue-range nil
+                      :lower-bounds [[0 0] [100 0]]
+                      :saturation-range [0 100]
+                      :brightness-range [0 0]}]
+      (is (= (define-color (:name monochrome)
+                           (:hue-range monochrome)
+                           (:lower-bounds monochrome)
+                           color-bounds) (conj color-bounds monochrome)))))
+
+  (testing "define-color: red"
+    (let [color-bounds '()
+          red {:name "red"
+               :hue-range [-26 18]
+               :lower-bounds [[20 100] [30 92] [40 89] [50 85] [60 78] [70 70] [80 60] [90 55] [100 50]]
+               :saturation-range [20 100]
+               :brightness-range [50 100]}]
+      (is (= (define-color (:name red)
+                           (:hue-range red)
+                           (:lower-bounds red)
+                           color-bounds) (conj color-bounds red)))))
+
+  (testing "define-color: orange"
+    (let [color-bounds '()
+          orange {:name "orange"
+                  :hue-range [19 46]
+                  :lower-bounds [[20 100] [30 93] [40 88] [50 86] [60 85] [70 70] [100 70]]
+                  :saturation-range [20 100]
+                  :brightness-range [70 100]}]
+      (is (= (define-color (:name orange)
+                           (:hue-range orange)
+                           (:lower-bounds orange)
+                           color-bounds) (conj color-bounds orange)))))
+
+  (testing "define-color: yellow"
+    (let [color-bounds '()
+          yellow {:name "yellow"
+                  :hue-range [47 62]
+                  :lower-bounds [[25 100] [40 94] [50 89] [60 86] [70 84] [80 82] [90 80] [100 75]]
+                  :saturation-range [25 100]
+                  :brightness-range [75 100]}]
+      (is (= (define-color (:name yellow)
+                           (:hue-range yellow)
+                           (:lower-bounds yellow)
+                           color-bounds) (conj color-bounds yellow)))))
+
+  (testing "define-color: green"
+    (let [color-bounds '()
+          green {:name "green"
+                 :hue-range [63 178]
+                 :lower-bounds [[30 100] [40 90] [50 85] [60 81] [70 74] [80 64] [90 50] [100 40]]
+                 :saturation-range [30 100]
+                 :brightness-range [40 100]}]
+      (is (= (define-color (:name green)
+                           (:hue-range green)
+                           (:lower-bounds green)
+                           color-bounds) (conj color-bounds green)))))
+
+  (testing "define-color: blue"
+    (let [color-bounds '()
+          blue {:name "blue"
+                :hue-range [47 62]
+                :lower-bounds [[20 100] [30 86] [40 80] [50 74] [60 60] [70 52] [80 44] [90 39] [100 35]]
+                :saturation-range [20 100]
+                :brightness-range [35 100]}]
+      (is (= (define-color (:name blue)
+                           (:hue-range blue)
+                           (:lower-bounds blue)
+                           color-bounds) (conj color-bounds blue)))))
+
+  (testing "define-color: purple"
+    (let [color-bounds '()
+          purple {:name "purple"
+                  :hue-range [258 282]
+                  :lower-bounds [[20 100] [30 87] [40 79] [50 70] [60 65] [70 59] [80 52] [90 45] [100 42]]
+                  :saturation-range [20 100]
+                  :brightness-range [42 100]}]
+      (is (= (define-color (:name purple)
+                           (:hue-range purple)
+                           (:lower-bounds purple)
+                           color-bounds) (conj color-bounds purple)))))
+
+  (testing "define-color: pink"
+    (let [color-bounds '()
+          pink {:name "pink"
+                :hue-range [283 334]
+                :lower-bounds [[20 100] [30 90] [40 86] [60 84] [80 80] [90 75] [100 73]]
+                :saturation-range [20 100]
+                :brightness-range [73 100]}]
+      (is (= (define-color (:name pink)
+                           (:hue-range pink)
+                           (:lower-bounds pink)
+                           color-bounds) (conj color-bounds pink))))))
